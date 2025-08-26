ac6cbc1d25fcfa3569486d41d0f295f9a3e0dd0a
theshowfantasy <ctarboxwork@gmail.com>
2025-08-25 20:15:21 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1128711965867663360"; // your league ID
 export const leagueName = "The Show Fantasy League"; // your league name
-export const dues = Alot; // (optional) used in template constitution page
+export const dues = 25000; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
@@ -9,7 +9,6 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 export const homepageText = `
   <p>The World Famous The Show Fantasy League </p>
   <p>Commissioner: Steven Cheah </p>
-
 `;
 
 /*   STEP 3   */
